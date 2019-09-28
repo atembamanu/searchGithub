c722fad53dccf3250043f40c68ea400789e072e5
atembamanu <atembaemm07@gmail.com>
2019-09-28 13:39:59 +0300

    add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
+import { AppComponent } from './app.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GithubSearchComponent } from './github-search/github-search.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: AppComponent},
+  { path: 'gtsearch', component: GithubSearchComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
