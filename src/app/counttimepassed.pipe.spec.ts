049c475911c60a2ed037e218f26fb5271730bb83
atembamanu <atembaemm07@gmail.com>
2019-09-30 06:52:33 +0300

    app custom pipe to determine how much time has passed since the last update

---
+++ src/app/counttimepassed.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { CountimePassed } from './counttimepassed.pipe';
+
+describe('RemoveHyphensPipe', () => {
+  it('create an instance', () => {
+    const pipe = new CountimePassed();
+    expect(pipe).toBeTruthy();
+  });
+});
