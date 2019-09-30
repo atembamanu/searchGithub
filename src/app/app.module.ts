7e94f5d12e771b6c6c0586f2245da56051946320
atembamanu <atembaemm07@gmail.com>
2019-09-30 06:54:49 +0300

    import and add newly created components  and imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,25 +4,37 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GithubSearchComponent } from './github-search/github-search.component';
-import { HighlightRecentRepoDirective } from './highlight-recent-repo.directive';
-import { RemoveHyphensPipe } from './remove-hyphens.pipe';
+import { CountimePassed } from './counttimepassed.pipe';
 import { NavbarComponent } from './navbar/navbar.component';
 import { GtprofileComponent } from './gtprofile/gtprofile.component';
+import { GtsearchService } from './gtsearch.service';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ConnectionService } from 'ng-connection-service';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { TextInputColorDirective } from './text-input-color.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     GithubSearchComponent,
-    HighlightRecentRepoDirective,
-    RemoveHyphensPipe,
+    CountimePassed,
     NavbarComponent,
-    GtprofileComponent
+    GtprofileComponent,
+    PageNotFoundComponent,
+    TextInputColorDirective
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    MatProgressSpinnerModule,
+
+
   ],
-  providers: [],
+  providers: [GtsearchService, ConnectionService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
