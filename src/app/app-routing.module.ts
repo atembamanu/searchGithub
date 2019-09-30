a1687f932d1a1f2e9e78dacd388b0b89571df2a6
atembamanu <atembaemm07@gmail.com>
2019-09-30 06:55:49 +0300

    import page not found from page not found component ts file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { GithubSearchComponent } from './github-search/github-search.component';
 import { GtprofileComponent } from './gtprofile/gtprofile.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 
 const routes: Routes = [
